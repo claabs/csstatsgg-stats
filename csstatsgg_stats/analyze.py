aa0de154f806100e66e3d26bc69ead6636a893c8
charlocharlie <charlielaabs@gmail.com>
2024-02-02 16:38:23 -0600

    Multi-map analysis

--- csstatsgg_stats/analyze.py
+++ csstatsgg_stats/analyze.py
@@ -7,7 +7,7 @@ import numpy as np
 maps = [
     "anubis",
     # "inferno",
-    # "mirage",
+    "mirage",
     # "vertigo",
     # "overpass",
     # "nuke",
@@ -16,80 +16,107 @@ maps = [
     # "office",
 ]
 
-map_name = "mirage"
+rank_names = {
+    1: "S1",
+    2: "S2",
+    3: "S3",
+    4: "S4",
+    5: "SE",
+    6: "SEM",
+    7: "GN1",
+    8: "GN2",
+    9: "GN3",
+    10: "GNM",
+    11: "MG1",
+    12: "MG2",
+    13: "MGE",
+    14: "DMG",
+    15: "LE",
+    16: "LEM",
+    17: "SMFC",
+    18: "GE",
+}
 
-f = open("output/" + map_name + "_ranks.json", "r").read()
-dataset: list[dict] = json.loads(f)
 
-f = open("output/" + map_name + "_wins.json", "r").read()
-# concat both datasets
-dataset += json.loads(f)
+def analyze_map(map_name):
+    dataset: list[dict] = []
 
-seen_urls = set()
-# chatGPT insanity
-unique_dataset = [
-    d
-    for d in dataset
-    if (
-        tuple(d.items())[:-1] not in seen_urls
-        and not seen_urls.add(tuple(d.items())[:-1])
-    )
-]
+    f = open("output/" + map_name + "_ranks.json", "r").read()
+    dataset += json.loads(f)
 
-rank_sorted: dict[int, list[float]] = {}
-for player in unique_dataset:
-    rank = player["current_rank"]
+    f = open("output/" + map_name + "_wins.json", "r").read()
+    # concat both datasets
+    dataset += json.loads(f)
 
-    if "won" not in player:
-        continue
-    total = player["won"] + player["lost"] + player["tied"]
-    winrate = (player["won"] + (player["tied"] / 2)) / total
+    seen_urls = set()
+    # chatGPT insanity
+    unique_dataset = [
+        d
+        for d in dataset
+        if (
+            tuple(d.items())[:-1] not in seen_urls
+            and not seen_urls.add(tuple(d.items())[:-1])
+        )
+    ]
 
-    if total >= 10 and rank > 0:
-        if rank not in rank_sorted:
-            rank_sorted[rank] = []
-        rank_sorted[rank].append(winrate)
+    rank_sorted: dict[int, list[float]] = {}
+    for player in unique_dataset:
+        rank = player["current_rank"]
 
-stats_dicts = []
-print(map_name)
+        if "won" not in player:
+            continue
+        total = player["won"] + player["lost"] + player["tied"]
+        winrate = (player["won"] + (player["tied"] / 2)) / total
 
-for rank, rank_data in sorted(rank_sorted.items()):
-    minimum = min(rank_data)
-    q1 = percentile_result = np.percentile(rank_data, 25)
-    mean = statistics.mean(rank_data)
-    q3 = percentile_result = np.percentile(rank_data, 75)
-    maximum = max(rank_data)
-    n = len(rank_data)
-    stdev = statistics.stdev(rank_data) if n > 1 else 0
-    print("%i: mean=%.2f, stdev=%.3f, n=%i" % (rank, mean, stdev, n))
-    stats_dicts.append(
-        {
-            "rank": rank,
-            "minimum": minimum,
-            "q1": q1,
-            "mean": mean,
-            "q3": q3,
-            "maximum": maximum,
-            "n": n,
-            "stdev": stdev,
-        }
-    )
+        if total >= 10 and rank > 0:
+            if rank not in rank_sorted:
+                rank_sorted[rank] = []
+            rank_sorted[rank].append(winrate)
 
-# Writing to CSV file
-fieldnames = [
-    "rank",
-    "minimum",
-    "q1",
-    "q3",
-    "maximum",
-    "mean",
-    "stdev",
-    "n",
-]
-with open("output/" + map_name + ".csv", "w", newline="") as csv_file:
-    csv_writer = csv.DictWriter(
-        csv_file,
-        fieldnames=fieldnames,
-    )
-    csv_writer.writeheader()
-    csv_writer.writerows(stats_dicts)
+    stats_dicts = []
+    print(map_name)
+
+    for rank, rank_data in sorted(rank_sorted.items()):
+        minimum = min(rank_data)
+        q1 = np.percentile(rank_data, 25)
+        mean = statistics.mean(rank_data)
+        q3 = np.percentile(rank_data, 75)
+        maximum = max(rank_data)
+        n = len(rank_data)
+        stdev = statistics.stdev(rank_data) if n > 1 else 0
+        print("%4s: mean=%.2f, stdev=%.3f, n=%i" % (rank_names[rank], mean, stdev, n))
+        stats_dicts.append(
+            {
+                "rank": rank_names[rank],
+                "minimum": minimum,
+                "q1": q1,
+                "mean": mean,
+                "q3": q3,
+                "maximum": maximum,
+                "n": n,
+                "stdev": stdev,
+            }
+        )
+
+    # Writing to CSV file
+    fieldnames = [
+        "rank",
+        "minimum",
+        "q1",
+        "q3",
+        "maximum",
+        "mean",
+        "stdev",
+        "n",
+    ]
+    with open("output/" + map_name + ".csv", "w", newline="") as csv_file:
+        csv_writer = csv.DictWriter(
+            csv_file,
+            fieldnames=fieldnames,
+        )
+        csv_writer.writeheader()
+        csv_writer.writerows(stats_dicts)
+
+
+for map_name in maps:
+    analyze_map(map_name)
